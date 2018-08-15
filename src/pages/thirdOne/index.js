3420ed27259c5c8b2712ed791690a954f20d3940
tenggouwa <17681875268@163.com>
2018-08-15 18:46:31 +0800

    搭起项目一览

--- src/pages/thirdOne/index.js
+++ src/pages/thirdOne/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './index.scss'
 
 export default class ThirdContant extends Component {
     constructor(props) {
@@ -9,9 +10,66 @@ export default class ThirdContant extends Component {
     componentDidMount() {
     }
     render() {
+        const dataList = [
+            {
+                key: 0,
+                src: '...',
+                link: '...',
+            },
+            {
+                key: 0,
+                src: '...',
+                link: '...',
+            },
+            {
+                key: 0,
+                src: '...',
+                link: '...',
+            },
+            {
+                key: 0,
+                src: '...',
+                link: '...',
+            },
+            {
+                key: 0,
+                src: '...',
+                link: '...',
+            }
+        ]
+        console.log(dataList)
         return (
-            <div style={this.props.style}>
-                Three
+            <div style={this.props.style} className="third">
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
+                <div className="third-gorup">
+                    <div />
+                    <p>17681875268</p>
+                    <button>复制地址</button>
+                </div>
             </div>
         )
     }
